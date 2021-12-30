8fedf7b935eaebdbaa34406b56fe38f7a55b4398
Fabio Ramos <fabio-s-ramos@hotmail.com>
2021-12-30 10:07:49 -0300

    layout and packages installed

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+<p align="center">
+<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
+
+## Projeto Laravel AdminLTE Ajax
+
+Web application with template adminlte in frontend and ajax for iteration with backend
+
+- Authentication
+- Roles and permissions
+- Modals without refresh page
+
+## License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,25 @@
+<aside class="control-sidebar control-sidebar-dark">
+  <div class="card bg-dark">
+    <div class="card-body box-profile">
+      <div class="text-center">
+        <img class="profile-user-img img-fluid img-circle" src="{{ asset('images/profile/user.png') }}" alt="User profile picture">
+      </div>
+
+      <h3 class="profile-username text-center">Nina Mcintire</h3>
+
+      <p class="text-muted text-center">Software Engineer</p>
+
+      <ul class="list-group bg-dark mb-3">
+        <li class="list-group-item">
+          <a class="d-flex align-items-center"> <i class="fa fa-lock pr-2"></i> <b>Senha</b> </a>
+        </li>
+        <li class="list-group-item">
+          <a class="d-flex align-items-center"> <i class="fa fa-user pr-2"></i> <b>Configurações</b> </a>
+        </li>
+      </ul>
+
+      <a href="#" class="btn btn-primary btn-block"><b>Desconectar</b></a>
+    </div>
+    <!-- /.card-body -->
+  </div>
+</aside>
